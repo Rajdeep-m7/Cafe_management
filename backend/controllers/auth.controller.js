7a44ee57ef881b91f4a8c132f1b319b54d4ecd78
rajdeep Majumdar <amazumder781@gmail.com>
2025-11-02 19:57:18 +0530

    fix some cokkie setttings

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -23,9 +23,8 @@ export const loginAdmin = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // true for https
-      sameSite: "lax",
-      maxAge: 24 * 60 * 60 * 1000,
+      secure: true,
+      sameSite: "none",
     });
 
     res.status(200).json({ message: "Login successful" });
